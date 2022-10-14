66fcc5d02e2b2f223820087d79a046c29ddf9626
Nico Spiller <nspiller@mailbox.org>
2022-10-14 18:32:45 -0400

    rewrite of the preprossing to return trials puts out intermediate list of valid trials (standard trials) fixed error in downsample function

--- utils.py
+++ utils.py
@@ -43,7 +43,7 @@ def convert_train(raw, ntot):
         idx = np.concatenate(raw) # flatten array
     except ValueError: # array appears to be already flattened
         idx = raw
-    idx -= 1 # start counting at 0
+    idx = idx - 1 # start counting at 0
 
     arr = np.zeros(ntot) # empty array with same size as xMM
     arr[idx] = 1 # set lick events as 1
@@ -51,61 +51,79 @@ def convert_train(raw, ntot):
     return arr
 
 
-def get_exp_data(matlab_file, downsample=1):
+def get_exp_data(matlab_file):
     '''Process matlab file stored on disk containing ephys experiment data
-
-    The time steps are downsampled to the interval given by the integer downsample
 
     Returns dictionary with the experimental features:
 
     read in ephys data stored as matlab struct
-    downsample by bin_size
 
     returns dict of experimental features'''
 
     m = loadmat(matlab_file, simplify_cells=True) # raw data from matlab
 
-    ntot = len(m["Track"]["xMM"]) # total number of time steps
+    # get start indices for trials
+    lap_idx = m['Track']['lapID']
+    trial_idx = np.unique(lap_idx) # array of unique trial ids
+    trial_idx = np.nonzero(trial_idx)[0] # trials start at 1
+
+    trial_start_idx = [] # list with trial start indices
+    for i in trial_idx:
+        s = np.where(lap_idx == i)[0] # index for first occurence of i
+        trial_start_idx.append(s[0])
+    else:
+        trial_start_idx.append(s[-1] + 1) # one after end of last trial
+    trial_start_idx = np.array(trial_start_idx)
+
+    # get total number of time steps
+    dst_raw = m["Track"]["xMM"]
+    ntot = len(dst_raw)
+
+    # split array of len=ntot into trials and drop data before first and after last trial
+    split_trials = lambda arr: np.split(arr, trial_start_idx)[1:-1] 
 
     # distance
-    dst = m["Track"]["xMM"]
-    dst = downsample_arr(dst, interval=downsample, mode='average')
+    dst = split_trials(dst_raw)
 
     # licks
     lck_raw = m["Laps"]["lickLfpInd"] 
     lck = convert_train(lck_raw, ntot) 
-    lck = downsample_arr(lck, interval=downsample, mode='sum') # downsample and sum
-    
+    lck = split_trials(lck)    
+
     # rewards
     rwd_raw = m["Laps"]["pumpLfpInd"]
 
     rwd_on_raw = np.array([i[0] for i in rwd_raw])
     rwd_on = convert_train(rwd_on_raw, ntot) 
-    rwd_on = downsample_arr(rwd_on, interval=downsample, mode='sum') 
+    rwd_on = split_trials(rwd_on)
 
     rwd_off_raw = np.array([i[1] for i in rwd_raw])
     rwd_off = convert_train(rwd_off_raw, ntot) 
-    rwd_off = downsample_arr(rwd_off, interval=downsample, mode='sum') 
+    rwd_off = split_trials(rwd_off) 
 
-    # cue and blackout
-    mov_raw = m["Laps"]["movieOnLfpInd"] # movie on times?
+    # standard and non-standard trials 
+    # TODO: this is specific to A026-20200323-01.mat and is used to filter out trials with different cue patterns
+    mov_raw = m["Laps"]["movieOnLfpInd"] # movie on times
+    mov_counts = np.array([ len(i) for i in mov_raw ]) # get number of movie on times per trial
+    std_trial_idx = np.where(mov_counts == 3)[0] # indices for standard trials
 
+    # cues
     cue_on_raw = np.array([i[0] for i in mov_raw]) # first two elements are cue on/off
     cue_on = convert_train(cue_on_raw, ntot) 
-    cue_on = downsample_arr(cue_on, interval=downsample, mode='sum')
+    cue_on = split_trials(cue_on)
 
     cue_off_raw = np.array([i[1] for i in mov_raw]) # first two elements are cue on/off
     cue_off = convert_train(cue_off_raw, ntot) # original code did idx-1 for cue off
-    cue_off = downsample_arr(cue_off, interval=downsample, mode='sum')
+    cue_off = split_trials(cue_off)
 
-    blk_on_raw = np.array([ i[2] for i in mov_raw ]) # third element is blk on (why do some have 4 elements?)
+    # blackout
+    blk_on_raw = np.array([ i[2] for i in mov_raw ]) # third element is blk on 
     blk_on = convert_train(blk_on_raw, ntot) 
-    blk_on = downsample_arr(blk_on, interval=downsample, mode='sum')
+    blk_on = split_trials(blk_on)
 
-    str_raw = m["Laps"]["startLfpInd"] # start trial?
-    blk_off_raw = np.array([ j for j in str_raw[1:]]) # next trial is blk off 
-    blk_off = convert_train(blk_off_raw, ntot)  # original code did idx-1 for blk off
-    blk_off = downsample_arr(blk_off, interval=downsample, mode='sum')
+    blk_off_raw = trial_start_idx[1:] # next trial is blk off
+    blk_off = convert_train(blk_off_raw, ntot) 
+    blk_off = split_trials(blk_off)
 
     # compile to dict
     raw_behavior = {
@@ -126,10 +144,11 @@ def get_exp_data(matlab_file, downsample=1):
     raw_spikes = dict() # dict with one spikes array per cluster
     for i in np.unique(clu):
         s_raw = spk_raw[np.where(clu == i )[0]] # spkikes from single single cluster
-        s = convert_train(s_raw, ntot) 
-        spk = downsample_arr(s, interval=downsample, mode='sum') # downsample and sum
+        spk = convert_train(s_raw, ntot)
+        spk = split_trials(spk)
 
         n = 'cluster_{}'.format(str(i)) # name for cluster
         raw_spikes[n] = spk
 
-    return raw_behavior, raw_spikes
+    return raw_behavior, raw_spikes, std_trial_idx
+    
